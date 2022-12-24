decffc2fb0a9efe0b3e946c7678f130bf7883f86
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-25 04:16:15 +0800

    feat: get jwt token from env

---
+++ env.ts
@@ -0,0 +1,9 @@
+import { cleanEnv, str } from "envalid";
+
+export const env = cleanEnv(Deno.env.toObject(), {
+  JWT_TOKEN: str({
+    desc: "your twitter token",
+    docs:
+      "https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2",
+  }),
+});
