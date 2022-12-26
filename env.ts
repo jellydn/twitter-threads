7407921910d355d9a8d14fd5ba3aa3ff661a096d
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-27 00:48:04 +0800

    fix: auto load file instead

--- env.ts
+++ env.ts
@@ -1,4 +1,4 @@
-import { config } from "dotenv/mod.ts";
+import "dotenv/load.ts";
 import { cleanEnv, str } from "envalid";
 import { logger } from "./logger.ts";
 
@@ -6,20 +6,17 @@ const report = (error: string) => {
   logger.error(error);
 };
 
-export const env = cleanEnv(
-  config(),
-  {
-    JWT_TOKEN: str({
-      desc: "your twitter token",
-      docs:
-        "https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2",
-    }),
+export const env = cleanEnv(Deno.env.toObject(), {
+  JWT_TOKEN: str({
+    desc: "your twitter token",
+    docs:
+      "https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2",
+  }),
+}, {
+  reporter: ({ errors, env }) => {
+    if (Object.keys(errors).length > 0) {
+      report("Invalid environment variables: " + Object.keys(errors));
+      logger.info(env);
+    }
   },
-  {
-    reporter: ({ errors }) => {
-      if (Object.keys(errors).length > 0) {
-        report("Invalid environment variables: " + Object.keys(errors));
-      }
-    },
-  },
-);
+});
