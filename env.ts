555b2ad25b47e6fb9ea00eeda891300706332c34
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-27 00:32:08 +0800

    feat: add env.ts
    - Add env.ts to load environment variables from .env file
    - Add .env.example to list all required environment variables

--- env.ts
+++ env.ts
@@ -6,16 +6,22 @@ const report = (error: string) => {
   logger.error(error);
 };
 
-export const env = cleanEnv(config(), {
-  JWT_TOKEN: str({
-    desc: "your twitter token",
-    docs:
-      "https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2",
+export const env = cleanEnv(
+  config({
+    safe: true, // Set to true to ensure that all necessary environment variables are defined after reading from .env. It will read .env.example to get the list of needed variables.
   }),
-}, {
-  reporter: ({ errors }) => {
-    if (Object.keys(errors).length > 0) {
-      report("Invalid environment variables: " + Object.keys(errors));
-    }
+  {
+    JWT_TOKEN: str({
+      desc: "your twitter token",
+      docs:
+        "https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2",
+    }),
   },
-});
+  {
+    reporter: ({ errors }) => {
+      if (Object.keys(errors).length > 0) {
+        report("Invalid environment variables: " + Object.keys(errors));
+      }
+    },
+  },
+);
